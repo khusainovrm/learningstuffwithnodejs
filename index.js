1b1d9f9da90b3f122333633284e692828a6e3263
Rinat Khusainov <khusainovrm@yandex.ru>
2020-06-10 10:14:39 +0300

    ENV

--- index.js
+++ index.js
@@ -9,15 +9,23 @@ const fileHome = path.resolve(__dirname, './public/home.html')
 const fileAbout = path.resolve(__dirname, './public/about.html')
 
 let writableStream = fs.createWriteStream('hello.txt')
-writableStream.write('\nHello from NodeJS')
-writableStream.write("That's it!")
-writableStream.end('End of the note')
+writableStream.write(`Hello from NodeJS at ${new Date().toLocaleString()}`)
+
+let newWritableStream = fs.createWriteStream('some.txt')
+let newWritableStreamUsingPipe = fs.createWriteStream('someForPipe.txt')
+
 let readableStream = fs.createReadStream('hello.txt', 'utf8')
+readableStream.on('data', chunk => newWritableStream.write(chunk));
+readableStream.pipe(newWritableStreamUsingPipe)
 
-readableStream.on('data', chunk => console.log(chunk));
-
-
+// static files, using middleware
 app.use(express.static(join(__dirname, 'public')))
+app.use('/about',(req,res,next) => {
+  console.log('It was a middleware', new Date().toLocaleString())
+  console.log('Selected page is ABOUT');
+  next()
+  }
+)
 
 app.get('/home', (req, res) => {
   fs.readFile(fileHome, (err, data) => {
@@ -33,6 +41,15 @@ app.get('/about', (req, res) => {
   })
 })
 
+app.get('/stuff', (req, res) => {
+  fs.readFile(path.resolve(__dirname, 'hello.txt'), (err, data) => {
+    if (err) throw err
+    // res.sendFile(path.resolve(__dirname, 'hello.txt'))
+    res.send('<h1>Something new</h1>')
 
+  })
+})
+
+console.log(process.env);
 
 app.listen(3000, () => console.log('Server has been started at', new Date().toLocaleTimeString()));
